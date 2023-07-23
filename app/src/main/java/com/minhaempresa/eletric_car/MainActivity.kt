e3cdf87c3fadde658ab0cea523cf2e23de5c1111
Daniela2319 <frankvt04@gmail.com>
2023-07-23 18:43:58 -0300

    Aula 1 Layout inicial do APP

---
+++ app/src/main/java/com/minhaempresa/eletric_car/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.minhaempresa.eletric_car
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+
+
+
+
+/* Log.d("texto preço ->" , preco.toString()) esse codigo serve para log e fazer testes
+   Log.d("texto digitado ->" , km.toString())
+   Log.d("Resultado ->" , resultado.toString())*/
