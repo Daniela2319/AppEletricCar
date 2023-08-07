b21547e413525a9c212ddd70eb097392f98a8f6e
Daniela2319 <frankvt04@gmail.com>
2023-08-07 20:00:19 -0300

    Aula 10 Manipulando entrada do usuário em app

--- app/src/main/java/com/minhaempresa/eletric_car/ui/CarFragment.kt
+++ app/src/main/java/com/minhaempresa/eletric_car/ui/CarFragment.kt
@@ -1,17 +1,29 @@
 package com.minhaempresa.eletric_car.ui
 
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Base64InputStream
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.loader.content.AsyncTaskLoader
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.minhaempresa.eletric_car.R
 import com.minhaempresa.eletric_car.data.CarFactory
 import com.minhaempresa.eletric_car.ui.adapter.CarAdapter
+import org.json.JSONArray
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 //Mostra uma lista no outro lado o detalhe da lista
 class CarFragment : Fragment(){
@@ -51,7 +63,50 @@ class CarFragment : Fragment(){
 
     fun setupListeners() {
         fab_Calcular.setOnClickListener {
-           startActivity(Intent(context, CalcularAutonomiaActivity::class.java))
+            MyTask().execute("https://igorbag.github.io/cars-api/cars.json")
+           //startActivity(Intent(context, CalcularAutonomiaActivity::class.java))
         }
     }
+
+    //esse código esta lendo dados
+inner class MyTask : AsyncTask<String, String, String>(){
+  fun onProExecute(){
+   super.onPreExecute()
+    Log.d("MyTask","Inciando...")
+}
+
+    override fun doInBackground(vararg url: String?): String {
+        var urlConnection: HttpURLConnection? = null
+        try {
+            val urlBase = URL(url[0])
+
+            urlConnection = urlBase.openConnection() as HttpURLConnection
+            urlConnection.connectTimeout = 60000
+            urlConnection.readTimeout = 60000
+
+            var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
+            publishProgress(response)
+        }catch (ex: Exception){
+            Log.e("Erro","Erro ao realizar processamento....")
+        }finally {
+                urlConnection?.disconnect()
+            }
+        return ""
+        }
+    }
+ //Dados ler atributos da api
+ fun onProgressUpdate(vararg values: String){
+     try {
+         val jsonArray = JSONTokener(values[0]).nextValue()as JSONArray
+         for (i in 0 until jsonArray.length()){
+
+         }
+
+     }catch (ex: java.lang.Exception){
+
+     }
+
+
+}
+
 }
