69a5e12e2a77a7e293df56cd753d30447bd8586d
Daniela2319 <frankvt04@gmail.com>
2023-08-10 20:10:42 -0300

    Aula 12 Manipulando entrada do usuário em app

--- app/src/main/java/com/minhaempresa/eletric_car/ui/CarFragment.kt
+++ app/src/main/java/com/minhaempresa/eletric_car/ui/CarFragment.kt
@@ -1,7 +1,12 @@
 package com.minhaempresa.eletric_car.ui
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.util.Base64InputStream
 import android.util.Log
@@ -9,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.loader.content.AsyncTaskLoader
 import androidx.recyclerview.widget.RecyclerView
@@ -27,10 +33,11 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 //Mostra uma lista no outro lado o detalhe da lista
-class CarFragment : Fragment(){
+class CarFragment : Fragment() {
 
     lateinit var fab_Calcular: FloatingActionButton
     lateinit var listaCarros: RecyclerView
+    lateinit var progress: ProgressBar
 
     var carrosArray: ArrayList<Carro> = ArrayList()
 
@@ -46,24 +53,31 @@ class CarFragment : Fragment(){
     //o android terminou de desenhar a tela para o usuário
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        callService()
         setupView(view)
         setupListeners()
+        val checkIntent = checkForInternet(context)
+        Log.d("Internet Connection", checkIntent.toString())
+        callService()
     }
 
-    fun setupView(view: View){
+    fun setupView(view: View) {
 
         view.apply {
             fab_Calcular = findViewById(R.id.fab_calcular)
             listaCarros = findViewById(R.id.rv_lista_carro)
+            progress = findViewById(R.id.pb_loader)
         }
     }
 
     fun setupList() {
-        val adapter = CarAdapter(CarFactory.list)
-        listaCarros.adapter = adapter
-    }
+        val carroAdapter = CarAdapter(carrosArray)
+        listaCarros.apply {
+            visibility = View.VISIBLE
+            listaCarros.adapter = carroAdapter
+        }
 
+
+    }
     fun setupListeners() {
         fab_Calcular.setOnClickListener {
 
@@ -71,82 +85,114 @@ class CarFragment : Fragment(){
         }
     }
 
-    fun callService(){
+
+    fun callService() {
         val urlBase = "https://igorbag.github.io/cars-api/cars.json"
-            MyTask().execute(urlBase)
+        MyTask().execute(urlBase)
+        progress.visibility = View.VISIBLE
+    }
+
+    //pega o serviço de conectividade fora do app
+    fun checkForInternet(context: Context?) : Boolean {
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when{
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION")
+           val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
     }
 
     //esse código esta lendo dados
-inner class MyTask : AsyncTask<String, String, String>(){
+    @SuppressLint("StaticFieldLeak")
+    inner class MyTask : AsyncTask<String, String, String>() {
 
-fun onProExecute(){
-   super.onPreExecute()
-    Log.d("MyTask","Inciando...")
-}
+        override fun onPreExecute() {
+            super.onPreExecute()
+            Log.d("MyTask", "Inciando...")
+        }
 
-    override fun doInBackground(vararg url: String?): String {
-        var urlConnection: HttpURLConnection? = null
-        try {
-            val urlBase = URL(url[0])
+        override fun doInBackground(vararg url: String?): String {
+            var urlConnection: HttpURLConnection? = null
+            try {
+                val urlBase = URL(url[0])
 
-            urlConnection = urlBase.openConnection() as HttpURLConnection
-            urlConnection.connectTimeout = 60000
-            urlConnection.readTimeout = 60000
-            urlConnection.setRequestProperty(
-                "Accept","application/json"
-            )
+                urlConnection = urlBase.openConnection() as HttpURLConnection
+                urlConnection.connectTimeout = 60000
+                urlConnection.readTimeout = 60000
+                urlConnection.setRequestProperty(
+                    "Accept", "application/json"
+                )
 
-            var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
-            publishProgress(response)
-        }catch (ex: Exception){
-            Log.e("Erro","Erro ao realizar processamento....")
-        }finally {
+                val responseCode = urlConnection.responseCode
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
+                    publishProgress(response)
+                } else {
+                    Log.e("Erro", "Serviço indisponivel no momento....")
+                }
+
+
+            } catch (ex: Exception) {
+                Log.e("Erro", "Erro ao realizar processamento....")
+            } finally {
                 urlConnection?.disconnect()
             }
-        return ""
+            return " "
+        }
+
+
+        //Dados lendo atributos da api
+        override fun onProgressUpdate(vararg values: String?) {
+            try {
+                val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
+
+                for (i in 0 until jsonArray.length()) {
+                    val id = jsonArray.getJSONObject(i).getString("id")
+                    Log.d("ID ->", id)
+
+                    val preco = jsonArray.getJSONObject(i).getString("preco")
+                    Log.d("Preco ->", preco)
+
+                    val bateria = jsonArray.getJSONObject(i).getString("bateria")
+                    Log.d("Bateria ->", bateria)
+
+                    val potencia = jsonArray.getJSONObject(i).getString("potencia")
+                    Log.d("Potencia ->", potencia)
+
+                    val recarga = jsonArray.getJSONObject(i).getString("recarga")
+                    Log.d("Recarga ->", recarga)
+
+                    val urlPhoto = jsonArray.getJSONObject(i).getString("urlPhoto")
+                    Log.d("urlPhoto ->", urlPhoto)
+
+                    val model = Carro(
+                        id = id.toInt(),
+                        preco = preco,
+                        bateria = bateria,
+                        potencia = potencia,
+                        recarga = recarga,
+                        urlPhoto = urlPhoto
+                    )
+
+                    carrosArray.add(model)
+                    Log.d("Model ->", model.toString())
+                }
+                progress.visibility = View.GONE
+                setupList()
+            } catch (ex: java.lang.Exception) {
+                Log.e("Erro ->", ex.message.toString())
+            }
         }
     }
- //Dados lendo atributos da api
- fun onProgressUpdate(vararg values: String){
-     try {
-         val jsonArray = JSONTokener(values[0]).nextValue()as JSONArray
-         for (i in 0 until jsonArray.length()){
-            val id = jsonArray.getJSONObject(i).getString("id")
-             Log.d("ID ->", id)
-
-             val preco = jsonArray.getJSONObject(i).getString("preco")
-             Log.d("Preco ->", preco)
-
-             val bateria = jsonArray.getJSONObject(i).getString("bateria")
-             Log.d("Bateria ->", bateria)
-
-             val potencia = jsonArray.getJSONObject(i).getString("potencia")
-             Log.d("Potencia ->", potencia)
-
-             val recarga = jsonArray.getJSONObject(i).getString("recarga")
-             Log.d("Recarga ->", recarga)
-
-             val urlPhoto = jsonArray.getJSONObject(i).getString("urlPhoto")
-             Log.d("urlPhoto ->", urlPhoto)
-
-             val model = Carro(
-                 id = id.toInt(),
-                 preco = preco,
-                 bateria = bateria,
-                 potencia = potencia,
-                 recarga = recarga,
-                 urlPhoto = urlPhoto
-             )
-
-             carrosArray.add(model)
-             Log.d("Model ->", model.toString())
-         }
-         setupList()
-     }catch (ex: java.lang.Exception){
-            Log.e("Erro ->", ex.message.toString())
-     }
-
-
-}
-
 }
